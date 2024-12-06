cddda8f9fec3bfc6c7e956ef65746d3a5c90c790
Lucop1911 <167803798+Lucop1911@users.noreply.github.com>
2024-12-06 15:24:14 +0100

    fixed issues

--- Projects/date_calculator/script.js
+++ Projects/date_calculator/script.js
@@ -80,14 +80,14 @@ document.getElementById('btn_converti').addEventListener('click', function () {
 });
 
 document.getElementById('num').addEventListener('keydown', (e) => {
-    if (isNaN(e.key) && e.key !== '.' && e.key !== ',') {
+    if (isNaN(e.key) && e.key !== '.' && e.key !== ',' && e.key !== '-') {
         e.preventDefault();
     }
 });
 
 document.getElementById('num').addEventListener('paste', (e) => {
     const pastedText = (e.clipboardData || window.Clipboard).getData("text");
-    if (isNaN(pastedText) && pastedText !== '.' && pastedText !== ',') {
+    if (isNaN(pastedText) && pastedText !== '.' && pastedText !== ',' && pastedText !== '-') {
         e.preventDefault();
     }
 });
